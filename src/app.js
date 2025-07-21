7f30373785cc25df79a3f354d0e595fc121abbe8
Gonzalo Triviño <gonzalotcp@gmail.com>
2025-07-21 19:15:29 +0000

    generador de nombre de dominios en el archivo app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+let pronouns = ['the','our','my','them'];
+let adjs = ['great','big','best','perfect'];
+let nouns = ['jogger','racoon','team','website'];
+let extensions = ['.com','.net','.org','.es']
+
+for (let pronoun of pronouns){
+  for (let adj of adjs) {
+    for (let noun of nouns) {
+      for (let extension of extensions) {
+        console.log(`${pronoun}${adj}${noun}${extension}`);      
+      }
+    }
+  }
+}
+
 };
+
+//Empecé usando un ciclo for declarando [i], pero vi que iba a ser más limpio usar directamente "for/of".
